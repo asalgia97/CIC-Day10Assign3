3072c98d966f9a008949cfe6cdd3bee118ff100b
anish <anishsalgia1997@gmail.com>
2020-12-11 21:23:01 +0530

    Refactor: Added code to check Head Tail using Random

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,20 @@
+#!/bin/bash -x
 echo "Welcome to FlipCoin Combination"
+flipCoin=y
+
+function checkCoin ()
+{
+	if [ $1 -eq 0 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+}
+
+while [ $flipCoin == y ]
+do
+	random=$(( RANDOM % 2 ))
+	checkCoin $random
+read -p "do you want to flip the coin again? (y/n): " flipCoin
+done
